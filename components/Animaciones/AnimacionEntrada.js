be28db13304dba960645c2e340c3fd1b338b8374
Guillermo Girolimini <69221188+ggirolimini@users.noreply.github.com>
2021-11-26 18:01:39 -0300

    Animación
    Se direcciono la animación a la imagen CLIMATE.png

--- components/Animaciones/AnimacionEntrada.js
+++ components/Animaciones/AnimacionEntrada.js
@@ -1,25 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Animated, View, StatusBar} from 'react-native';
-import imagen from '../../assets/images/HELADOS.png';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Animated, View, StatusBar } from "react-native";
+import imagen from "../../assets/images/CLIMATE.png";
 
-
-const AnimacionEntrada = ({setAnimated}) => {
-   
-
-   /* const [position] = useState(new Animated.Value(0));
+const AnimacionEntrada = ({ setAnimated }) => {
+  /* const [position] = useState(new Animated.Value(0));
    const [position1] = useState(new Animated.Value(000));
    const [position2] = useState(new Animated.Value(000)); */
-   const [show] = useState(new Animated.Value(0));
-   const [font] = useState(new Animated.Value(1));
+  const [show] = useState(new Animated.Value(0));
+  const [font] = useState(new Animated.Value(1));
 
-   useEffect(() => {
-     Animated.parallel([
+  useEffect(() => {
+    Animated.parallel([
       /*  Animated.timing(position, {
          toValue: 0,
          duration: 2000,
          useNativeDriver: false,
        }), */
-       /* Animated.timing(position1, {
+      /* Animated.timing(position1, {
         toValue: -900,
         duration: 2000,
         delay: 500,
@@ -31,37 +28,39 @@ const AnimacionEntrada = ({setAnimated}) => {
         delay: 1000,
         useNativeDriver: false,
       }), */
-       Animated.timing(show, {
-         toValue: 1,
-         duration: 2500,
-         //delay: 2000,
-         useNativeDriver: false,
-       }),
-     ]).start(() =>
-       Animated.timing(font, {
-         toValue: 1.3,
-         duration: 2500,
+      Animated.timing(show, {
+        toValue: 1,
+        duration: 2500,
+        //delay: 2000,
+        useNativeDriver: false,
+      }),
+    ]).start(() =>
+      Animated.timing(font, {
+        toValue: 1.3,
+        duration: 2500,
         // delay: 1000,
-         useNativeDriver: false,
-       }).start(() => setAnimated(true)),
-     );
-   }, []);
+        useNativeDriver: false,
+      }).start(() => setAnimated(true))
+    );
+  }, []);
 
-   
-     return (
-       <>
-         <StatusBar
-           animated={true}
-           backgroundColor='#F0A500'
-           barStyle="light-content"
-         />
-         <View style={styles.container}>
-           <Animated.Image
-             style={[styles.image, {opacity: show , transform: [{scale: font}] }]}
-             source={imagen}
-           />
+  return (
+    <>
+      <StatusBar
+        animated={true}
+        backgroundColor="#F0A500"
+        barStyle="light-content"
+      />
+      <View style={styles.container}>
+        <Animated.Image
+          style={[
+            styles.image,
+            { opacity: show, transform: [{ scale: font }] },
+          ]}
+          source={imagen}
+        />
 
-         {/*    <Animated.Image
+        {/*    <Animated.Image
              style={[styles.image, {top: position1,  right: 0}]}
              source={imagen}
            />
@@ -71,41 +70,34 @@ const AnimacionEntrada = ({setAnimated}) => {
              source={imagen}
            /> */}
 
-
-           {/* <Animated.Text
+        {/* <Animated.Text
              style={[styles.text, {opacity: show, transform: [{scale: font}]}]}>
             Hola!
 
            </Animated.Text> */}
-
-
-         </View>
-       </>
-     );
-  
+      </View>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     alignItems: 'center',
-     backgroundColor: 'rgba(125, 125, 125, 0.2)',
-     justifyContent: 'center'
-    
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "rgba(125, 125, 125, 0.2)",
+    justifyContent: "center",
+  },
 
-   image: {
-     width: '80%',
-     //height: '50%',
-     resizeMode: 'contain',
-    
-   },
+  image: {
+    width: "80%",
+    //height: '50%',
+    resizeMode: "contain",
+  },
 
-   text: {
-     fontSize: 40,
-     color: '#F0A500',
-   },
+  text: {
+    fontSize: 40,
+    color: "#F0A500",
+  },
 });
 
 export default AnimacionEntrada;
-
